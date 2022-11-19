ff3fc99faf58492e81455713d68066168536aaae
Yeojin Kim <yeojin.dev@gmail.com>
2022-11-19 10:12:16 +0900

    feat: use FutureTask

--- src/main/kotlin/com/yeojin/packetbird/PacketbirdApplication.kt
+++ src/main/kotlin/com/yeojin/packetbird/PacketbirdApplication.kt
@@ -1,9 +1,12 @@
 package com.yeojin.packetbird
 
 import java.util.concurrent.Executors
+import java.util.concurrent.FutureTask
+
+const val REPEAT_NUMBER = 10
 
 fun mainThread() {
-    while (true) {
+    repeat(REPEAT_NUMBER) {
         println("Hello Thread! ${Thread.currentThread().name}")
     }
 }
@@ -11,8 +14,19 @@ fun mainThread() {
 fun main(args: Array<String>) {
     val threadAmount = args[0].toInt()
     val executor = Executors.newFixedThreadPool(threadAmount)
-    val worker = Runnable { mainThread() }
-    executor.execute(worker)
+    val task1 = FutureTask<Int>(::mainThread, 1)
+    val task2 = FutureTask<Int>(::mainThread, 2)
+
+    executor.submit(task1)
+    executor.submit(task2)
+
+    while (true) {
+        println("Checking...")
+        if (task1.isDone && task2.isDone) {
+            println("Result.. task1 ${task1.get()} | task2 ${task2.get()}")
+            break
+        }
+    }
 
     println("Hello World!")
 }
